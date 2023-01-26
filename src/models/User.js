11b7c575bb7e56ea381f035758f800426df35751
nachotrossero <nachotrossero@gmail.com>
2023-01-26 19:37:26 -0300

    comiteo algunos cambios que quedaron pendientes

--- src/models/User.js
+++ src/models/User.js
@@ -9,7 +9,6 @@ const User = {
         const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
         return users;
     },
-
     generateId: function(){
         let allUsers = this.findAll();
         let lastUser = allUsers.pop();
